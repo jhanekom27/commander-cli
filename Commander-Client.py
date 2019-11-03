9e11fd37f9d5d77e96f059dcc1e04a0b743f6516
Roy Lee <roy.quan.kuro.lee@gmail.com>
2019-11-03 10:30:54 +0800

    Beginning py script

---
+++ Commander-Client.py
@@ -0,0 +1,4 @@
+#Commander Client
+
+#Our beginning commander utility tool for tracking all MTG Commander things.
+#Our little practise project to boost our python skills. Starting with a simple command line interface and then expand to more complex things.
